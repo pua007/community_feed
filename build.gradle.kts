05744f834f8f35911eb8731847ac1e5de72c1f14
pua007 <pua002@naver.com>
2024-10-07 18:16:28 +0900

    20204-10-07 feat 스프링부트 플러그인 추가, 스프링 스타터, 스프링스타터 웹 등 추가, Main -> Application으로 이름 변경 및 @SpringBootApplication어노테이션 추가 메인 메서드에 SpringApplication.run(Application.class); 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "org.fastcampus"
@@ -10,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
