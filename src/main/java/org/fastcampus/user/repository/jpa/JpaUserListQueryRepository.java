ee1f7bc0c53a0124dff67ef0a8a03775ddc7421f
pua007 <pua002@naver.com>
2024-10-31 18:21:46 +0900

    2024-10-31

--- src/main/java/org/fastcampus/user/repository/jpa/JpaUserListQueryRepository.java
+++ src/main/java/org/fastcampus/user/repository/jpa/JpaUserListQueryRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface JpaUserListQueryRepository extends JpaRepository<UserEntity, Long>{
     //:을 통해서 넘겨주는 userId변수임을 나타내주면 된다.
+
+    //Dto로 받지 않고 값으로도 받을수 있다 u, ur 만약 특정 데이터베이스에서 제공하는 쿼리문을 쓰고 싶다면 nativeQuery = true 옵션을 주고 쓰면된다.
     @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage)"
             +" FROM UserRelationEntity ur"
             +" INNER JOIN UserEntity u ON ur.followerUserId = u.id"
@@ -18,6 +20,6 @@ public interface JpaUserListQueryRepository extends JpaRepository<UserEntity, Lo
     @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage)"
             +" FROM UserRelationEntity ur"
             +" INNER JOIN UserEntity u ON ur.followingUserId = u.id"
-            +" WHERE ur.followerUserId = :userId")
+            +" WHERE ur.followerUserId = :userId" , nativeQuery = true)
     List<GetUserListResponseDto>getFollowerUserList(Long userId);
 }
--- src/main/java/org/fastcampus/user/repository/jpa/JpaUserRepository.java
+++ src/main/java/org/fastcampus/user/repository/jpa/JpaUserRepository.java
@@ -2,6 +2,6 @@ package org.fastcampus.user.repository.jpa;
 
 import org.fastcampus.user.repository.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//JpaRepository<가지고와야하는 대상의 엔티티, 그 엔티티의 아이디값>
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
 }
